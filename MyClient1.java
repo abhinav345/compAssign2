8b491e99af11d522fe74ad8ecbad363f16d95a48
abhinav345 <abhinavmadhusudan.reddy@students.mq.edu.au>
2022-06-05 19:33:13 +1000

    experementing with EJWT

--- MyClient1.java
+++ MyClient1.java
@@ -11,20 +11,107 @@ class MyClient1 {
 
         String str;
 
+        System.out.println("Client Says: HELO");
         dout.write(("HELO\n").getBytes());
         dout.flush();
         str = din.readLine();
         System.out.println("Server Says: " + str);
 
+        System.out.println("Client Says: AUTH");
         String username = System.getProperty("user.name");
         dout.write(("AUTH " + username + "\n").getBytes());
         dout.flush();
         str = din.readLine();
         System.out.println("Server Says: " + str);
 
+        System.out.println("Client Says: REDY ");
         dout.write(("REDY\n").getBytes());
         dout.flush();
         str = din.readLine();
         System.out.println("Server Says: " + str);
+        String[] jobSplit = str.split(" ");
+        String jobType = jobSplit[0];
+
+      //  while(!(jobType.equalsIgnoreCase("none"))) {
+            System.out.print("Client Says: ");
+            System.out.println("GETS Capable " + jobSplit[4] + "  " + jobSplit[5] + " " + jobSplit[6]);
+            dout.write(("GETS Capable " + jobSplit[4] + " " + jobSplit[5] + " " + jobSplit[6] + "\n").getBytes());
+            dout.flush();
+            str = din.readLine();
+            System.out.println("Server says " + str);
+            String[] data = str.split(" ");
+            int numServers = Integer.parseInt(data[1]);
+
+            dout.write(("OK\n").getBytes());
+            dout.flush();
+
+            System.out.println("Servers are: ");
+            for(int i = 0;i < numServers; i++) {
+                str = din.readLine();
+                System.out.println("Server " + (i+1) + " is " + str);
+            }
+
+            dout.write(("OK\n").getBytes());
+            dout.flush();
+            str = din.readLine();
+            System.out.println("Server Says: " + str);
+
+            dout.write(("EJWT tiny 0 \n").getBytes());
+            str = din.readLine();
+            System.out.println("Server says(Time): " + str);
+
+            dout.write(("SCHD 0 tiny 0 \n").getBytes());
+            str = din.readLine();
+            System.out.println("Server says(Schd Info): " + str);
+
+            System.out.println("Client Says: REDY ");
+            dout.write(("REDY\n").getBytes());
+            dout.flush();
+            str = din.readLine();
+            System.out.println("Server Says: " + str);
+            jobSplit = str.split(" ");
+            jobType = jobSplit[0];
+
+            System.out.print("Client Says: ");
+            System.out.println("GETS Capable " + jobSplit[4] + "  " + jobSplit[5] + " " + jobSplit[6]);
+            dout.write(("GETS Capable " + jobSplit[4] + " " + jobSplit[5] + " " + jobSplit[6] + "\n").getBytes());
+            dout.flush();
+            str = din.readLine();
+            System.out.println("Server says " + str);
+            data = str.split(" ");
+            numServers = Integer.parseInt(data[1]);
+
+            dout.write(("OK\n").getBytes());
+            dout.flush();
+
+            System.out.println("Servers are: ");
+            for(int i = 0;i < numServers; i++) {
+                str = din.readLine();
+                System.out.println("Server " + (i+1) + " is " + str);
+            }
+
+            dout.write(("OK\n").getBytes());
+            dout.flush();
+            str = din.readLine();
+            System.out.println("Server Says: " + str);
+
+            dout.write(("EJWT tiny 0 \n").getBytes());
+            str = din.readLine();
+            System.out.println("Server says(Time): " + str);
+
+            dout.write(("EJWT tiny 1 \n").getBytes());
+            str = din.readLine();
+            System.out.println("Server says(Time): " + str);
+
+            dout.write("QUIT\n".getBytes());
+            dout.flush();
+            str = din.readLine();
+            System.out.println("Server Says: " + str);
+
+            din.close();
+            dout.close();
+            s.close();
+       // }
+
     }
 }
